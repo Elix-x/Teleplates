0a768ec6924b80eff787b92db9311cf06987ce87
elix-x <agent-lego@mail.ru>
2016-02-20 15:24:35 +0100

    1.2.2 - Excore 1.3.8 compatibility, crash fixes
    -Excore 1.3.8 compatibility.
    -Crash fixes:
    --Fixed #7 .
    --Fixed #8 .
    -Code cleanup.

--- src/main/java/code/elix_x/mods/teleplates/TeleplatesBase.java
+++ src/main/java/code/elix_x/mods/teleplates/TeleplatesBase.java
@@ -1,10 +1,10 @@
 package code.elix_x.mods.teleplates;
 
-import java.util.function.Function;
-
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import com.google.common.base.Function;
 
 import code.elix_x.excore.EXCore;
 import code.elix_x.excore.utils.packets.SmartNetworkWrapper;
@@ -42,7 +42,7 @@ public class TeleplatesBase {
 
 	public static final String MODID = "teleplates";
 	public static final String NAME = "Teleplates";
-	public static final String VERSION = "1.2.1";
+	public static final String VERSION = "1.2.2";
 
 	public static final Logger logger = LogManager.getLogger(NAME);
 
