90ad90b7c29206698c5c539f0b9ec0c75ef1a92f
elix-x <agent-lego@mail.ru>
2016-02-28 17:30:45 +0100

    1.2.6 - Finished & fixed renderer, bug fixes
    -Finished new teleplates renderer.
    -Flattened teleplates even more.
    -Added teleplates synchronization (of tile entities).
    -Bug fixes:
    --Fixed #15.
    -Code cleanup.

--- src/main/java/code/elix_x/mods/teleplates/TeleplatesBase.java
+++ src/main/java/code/elix_x/mods/teleplates/TeleplatesBase.java
@@ -42,7 +42,7 @@ public class TeleplatesBase {
 
 	public static final String MODID = "teleplates";
 	public static final String NAME = "Teleplates";
-	public static final String VERSION = "1.2.5";
+	public static final String VERSION = "1.2.6";
 
 	public static final Logger logger = LogManager.getLogger(NAME);
 
