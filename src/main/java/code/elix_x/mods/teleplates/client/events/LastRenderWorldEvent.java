f2d05315eaf3f035c2f3060291d5caf008700156
elix-x <agent-lego@mail.ru>
2016-05-07 12:08:18 +0200

    Fixed possible rendering glitches
    Fixed possible rendering glitches.

--- src/main/java/code/elix_x/mods/teleplates/client/events/LastRenderWorldEvent.java
+++ src/main/java/code/elix_x/mods/teleplates/client/events/LastRenderWorldEvent.java
@@ -1,12 +1,13 @@
 package code.elix_x.mods.teleplates.client.events;
 
 import code.elix_x.mods.teleplates.client.renderer.tileentity.TileEntityTeleplateRenderer;
+import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 
 public class LastRenderWorldEvent {
 
-	@SubscribeEvent
+	@SubscribeEvent(priority = EventPriority.LOW)
 	public void render(RenderWorldLastEvent event){
 		TileEntityTeleplateRenderer.renderAllTeleplates(event.context.tileEntities);
 	}
