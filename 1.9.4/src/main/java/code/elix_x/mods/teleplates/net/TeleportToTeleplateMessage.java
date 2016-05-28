c298c0534ad9cb8f06d716872f47fb124f60b312
elix-x <agent-lego@mail.ru>
2016-05-28 21:28:48 +0200

    Started working on password sever check
    Started working on password sever check.

--- 1.9.4/src/main/java/code/elix_x/mods/teleplates/net/TeleportToTeleplateMessage.java
+++ 1.9.4/src/main/java/code/elix_x/mods/teleplates/net/TeleportToTeleplateMessage.java
@@ -1,28 +1,33 @@
 package code.elix_x.mods.teleplates.net;
 
 import io.netty.buffer.ByteBuf;
+import net.minecraftforge.fml.common.network.ByteBufUtils;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 
 public class TeleportToTeleplateMessage implements IMessage {
 
 	public int teleplate;
+	public String password;
 
 	public TeleportToTeleplateMessage(){
 
 	}
 
-	public TeleportToTeleplateMessage(int teleplateId){
+	public TeleportToTeleplateMessage(int teleplateId, String password){
 		this.teleplate = teleplateId;
+		this.password = password;
 	}
 
 	@Override
 	public void fromBytes(ByteBuf buf){
 		teleplate = buf.readInt();
+		password = ByteBufUtils.readUTF8String(buf);
 	}
 
 	@Override
 	public void toBytes(ByteBuf buf){
 		buf.writeInt(teleplate);
+		ByteBufUtils.writeUTF8String(buf, password);
 	}
 
 }
